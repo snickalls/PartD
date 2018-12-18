5210040c5d74a7fe608c47a4bd49473089230790
snickalls <socketdesignlab@gmail.com>
2018-12-18 21:57:50 +1300

    Third Commit

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -4,6 +4,7 @@ public class PartD {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Stephen Francis Nickalls");
 		System.out.println("My favourite colour is Blue");
+		System.out.println("My favourite mode of transport is imagination");
 
 	}
 

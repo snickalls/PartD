b9cff346d652150b21714ae08682f117a929a8b7
snickalls <socketdesignlab@gmail.com>
2018-12-18 22:12:36 +1300

    Added readme

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -2,7 +2,7 @@
 public class PartD {
 
 	public static void main(String[] args) {
-		System.out.println("Hi, my name is Stephen Francis Nickalls");
+		System.out.println("Hi, my name is Stephen Nickalls");
 		System.out.println("My favourite colour is Blue");
 		System.out.println("My favourite mode of transport is imagination");
 		System.out.println("The best day of the week is today");
